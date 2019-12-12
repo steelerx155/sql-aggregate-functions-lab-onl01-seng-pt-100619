b8472dcc4458501d324679537b827c4b966e20b0
Randy Steele <steelerx155@gmail.com>
2019-12-12 18:49:38 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,12 +9,12 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  "SELECT AVG(gpa) FROM students"
+  "SELECT AVG(gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
 end
 
 def average_gpa_for_9th_grade
-  "SELECT tardies FROM students"
+  "SELECT tardies FROM students;"
 end
