bc18faf1c435e2b5cb7a8c6c9b0020561232bed7
Randy Steele <steelerx155@gmail.com>
2019-12-12 18:41:07 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,7 +1,7 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
 def highest_student_gpa
-  SELECT gpa from sutdens LIMIT 
+  SELECT * from sutdens LIMIT 
 end
 
 def lowest_student_gpa
